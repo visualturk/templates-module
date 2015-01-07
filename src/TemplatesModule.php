53e59a5bb12928f417967c5c22c12f7b514e0615
Ryan Thompson <service@aiwebsystems.com>
2015-01-06 23:40:09 -0600

    nav

--- src/TemplatesModule.php
+++ src/TemplatesModule.php
@@ -13,4 +13,10 @@ use Anomaly\Streams\Platform\Addon\Module\Module;
 class TemplatesModule extends Module
 {
 
+    /**
+     * The module's navigation group.
+     *
+     * @var null
+     */
+    protected $navigation = 'streams::navigation.structure';
 }
